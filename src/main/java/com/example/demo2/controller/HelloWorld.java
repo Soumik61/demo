d15bdac8f0be6991c1fdd8277ce690a84582fe43
Soumik Singh <Soumik.Singh@gds.ey.com>
2023-09-07 13:31:57 +0530

    first commit

---
+++ src/main/java/com/example/demo2/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
